5c398b71db56bb3b3cfc5c0667e55b96046d70a9
Marius <mariustsiorimbola@gmail.com>
2023-12-01 23:45:59 +0300

    commit

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-import dotenv from "vite-plugin-dotenv"
 
 import { defineConfig,loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -8,9 +7,9 @@ import process from 'node:process'
 // https://vitejs.dev/config/
 export default defineConfig(({mode})=>{
   const env = loadEnv(mode, process.cwd(), '')
-  return {
+    return {
     plugins: [
-      vue(),dotenv()
+      vue()
     ],
     resolve: {
       alias: {
